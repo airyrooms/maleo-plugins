8551299585796f9f723f523706a5cf045f121a0f
Alvin Lim <alvink77@gmail.com>
2019-03-26 15:38:47 +0700

    chore(travis): discord webhook alert

--- ci/publish-stable.sh
+++ ci/publish-stable.sh
@@ -4,8 +4,18 @@ yarn publish:prepublish
 # publish stable manually
 echo "Bumping version"
 yarn publish:version-stable
-echo "Publishing Stable Version to NPM"
-yarn publish:stable
+
+echo "Publishing Plugins to NPM. More info $TRAVIS_BUILD_WEB_URL"
+if ! yarn publish:stable; then
+  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/verbose/send.sh
+  chmod +x send.sh
+  ./send.sh failure $DISCORD_WEBHOOK_URL
+else
+  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/verbose/send.sh
+  chmod +x send.sh
+  ./send.sh success $DISCORD_WEBHOOK_URL
+fi
+
 echo "Commit Git Refs"
 git add .
 git commit --amend --no-edit
