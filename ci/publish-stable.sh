c7c67e1f03928e8c787e01ee24326931222299da
Alvin Lim <alvink77@gmail.com>
2019-03-26 15:32:57 +0700

    chore(travis): automate publish

--- ci/publish-stable.sh
+++ ci/publish-stable.sh
@@ -2,6 +2,11 @@ echo "Publishing Stable"
 echo "Running Prepublish"
 yarn publish:prepublish
 # publish stable manually
-# yarn publish:version-stable
+echo "Bumping version"
+yarn publish:version-stable
 echo "Publishing Stable Version to NPM"
 yarn publish:stable
+echo "Commit Git Refs"
+git add .
+git commit --amend --no-edit
+git push origin master
